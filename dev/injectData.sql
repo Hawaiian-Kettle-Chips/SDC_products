227a8c917aad7bc859b7d991c7624f567ca02c01
August <AedonGrunGott@users.noreply.github.com>
2023-02-16 11:52:49 -0800

    move to pooling, and data injection OK

---
+++ dev/injectData.sql
@@ -0,0 +1,11 @@
+-- psql -d products_api -a -f injectData.sql -U postgres
+
+\c products_api;
+
+\copy products from 'raw_data/product.csv' delimiter ',' csv header;
+\copy features from 'raw_data/features.csv' delimiter ',' csv header;
+-- NULL AS is for converting sting in CSV to a DOUBLE
+\copy styles from 'raw_data/styles.csv' delimiter ',' csv NULL AS 'null' header;
+\copy skus from 'raw_data/skus.csv' delimiter ',' csv header;
+\copy photos from 'raw_data/photos.csv' delimiter ',' csv header;
+\copy relations from 'raw_data/related.csv' delimiter ',' csv header;
--- dev/schema.sql
+++ dev/schema.sql
@@ -1,72 +1,80 @@
--- PostgreSQL Schema
+-- PostgreSQL Schema'
+-- `psql -f schema.sql -p <port_int> -U <user>`
 
 DROP DATABASE IF EXISTS products_api;
 CREATE DATABASE products_api;
 
-USE products_api;
+-- psql equivelent of `USE products_api;`
+\c products_api;
 
-CREATE TABLE 'products' {
-    'id' BIGINT NOT NULL,
-    'name' TEXT NOT NULL,
-    'slogan' TEXT,
-    'description' TEXT,
-    'category' TEXT,
-    'default_price' DOUBLE PRECISION,
-    PRIMARY KEY ('id', 'name')
-};
+-- id,name,slogan,description,category,default_price
+CREATE TABLE products (
+    id BIGINT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    slogan TEXT,
+    description TEXT,
+    category TEXT,
+    default_price DOUBLE PRECISION,
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE 'features' {
-    'id' BIGINT NOT NULL,
-    'product_id' BIGINT NOT NULL,
-    'feature' TEXT NOT NULL,
-    'value' TEXT ,
-    PRIMARY KEY ('id'),
-    CONSTRAINT 'foreign_key_features'
-        FOREIGN KEY ('product_id')
-        REFERENCES 'products' ('id')
-};
+-- id,product_id,feature,value
+CREATE TABLE features (
+    id BIGINT NOT NULL UNIQUE,
+    product_id BIGINT NOT NULL,
+    feature TEXT NOT NULL,
+    value TEXT,
+    PRIMARY KEY (id),
+    CONSTRAINT foreign_key_features
+        FOREIGN KEY (product_id)
+        REFERENCES products (id)
+);
 
-CREATE TABLE 'styles' {
-    'id' BIGINT NOT NULL,
-    'product_id' BIGINT NOT NULL,
-    'name' TEXT NOT NULL,
-    'original_price' DOUBLE PRECISION,
-    'sale_price' DOUBLE PRECISION,
-    'default?' BOOLEAN,
-    PRIMARY KEY ('id'),
-    CONSTRAINT 'foreign_key_styles'
-        FOREIGN KEY ('product_id')
-        REFERENCES 'products' ('id')
-};
+-- id,productId,name,sale_price,original_price,default_style
+CREATE TABLE styles (
+    id BIGINT NOT NULL UNIQUE,
+    productId BIGINT NOT NULL,
+    name TEXT NOT NULL,
+    sale_price DOUBLE PRECISION,
+    original_price DOUBLE PRECISION,
+    default_style BOOLEAN,
+    PRIMARY KEY (id),
+    CONSTRAINT foreign_key_styles
+        FOREIGN KEY (productId)
+        REFERENCES products (id)
+);
 
-CREATE TABLE 'photos' {
-    'id' BIGINT NOT NULL,
-    'style_id' BIGINT NOT NULL,
-    'thumbnail_url' TEXT,
-    'url' TEXT,
-    PRIMARY KEY ('id'),
-    CONSTRAINT 'foreign_key_photos'
-        FOREIGN KEY ('style_id')
-        REFERENCES 'styles' ('id')
-};
+-- id,styleId,url,thumbnail_url
+CREATE TABLE photos (
+    id BIGINT NOT NULL UNIQUE,
+    styleId BIGINT NOT NULL,
+    thumbnail_url TEXT,
+    url TEXT,
+    PRIMARY KEY (id),
+    CONSTRAINT foreign_key_photos
+        FOREIGN KEY (styleId)
+        REFERENCES styles (id)
+);
 
-CREATE TABLE 'skus' {
-    'id' BIGINT NOT NULL,
-    'style_id' BIGINT NOT NULL,
-    'size' TEXT NOT NULL,
-    'quantity' BIGINT,
-    PRIMARY KEY ('id'),
-    CONSTRAINT 'foreign_key_skus'
-        FOREIGN KEY ('style_id')
-        REFERENCES 'styles' ('id')
-};
+-- id,styleId,size,quantity
+CREATE TABLE skus (
+    id BIGINT NOT NULL UNIQUE,
+    styleId BIGINT NOT NULL,
+    size TEXT NOT NULL,
+    quantity BIGINT,
+    PRIMARY KEY (id),
+    CONSTRAINT foreign_key_skus
+        FOREIGN KEY (styleId)
+        REFERENCES styles (id)
+);
 
-CREATE TABLE 'relations' {
-    'id' BIGINT NOT NULL,
-    'product_id' BIGINT NOT NULL,
-    'related_id' BIGINT NOT NULL,
-    PRIMARY KEY ('id', 'product_id', 'related_id'),
-    CONSTRAINT 'foreign_key_relations'
-        FOREIGN KEY ('product_id')
-        REFERENCES 'products' ('id')
-};
+-- id,current_product_id,related_product_id
+CREATE TABLE relations (
+    id BIGINT NOT NULL UNIQUE,
+    current_product_id BIGINT NOT NULL,
+    related_product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT foreign_key_relations
+        FOREIGN KEY (current_product_id)
+        REFERENCES products (id)
+);
